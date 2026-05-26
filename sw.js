cc1d084177ce9ef5a89df5b6b07590b12053b37f
alaztito0z <dark.ast0z@gmail.com>
2026-05-26 11:45:01 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
-self.addEventListener('install', e => e.waitUntil(caches.open('soul-v1').then(c => c.addAll([
+self.addEventListener('install', e => e.waitUntil(caches.open('soul-v2').then(c => c.addAll([
     '/',
     '/index.html',
     '/catalogo.html',
+    '/admin.html',
     '/style.css',
     '/catalogo.css',
+    '/admin.css',
     '/main.js',
     '/catalogo.js',
-    '/shared.js'
+    '/shared.js',
+    '/admin.js'
 ]))));
 
 self.addEventListener('fetch', e => e.respondWith(
